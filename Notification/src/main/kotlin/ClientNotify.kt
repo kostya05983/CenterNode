ec51a95e36fb3af0484c94cabf23023ee82c0e13
konstantin.volivach <konstantin.volivach@eltex.loc>
2019-01-22 08:20:12 +0700

    Add broadcast

--- Notification/src/main/kotlin/ClientNotify.kt
+++ Notification/src/main/kotlin/ClientNotify.kt
@@ -2,18 +2,51 @@ import java.io.IOException
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
+import java.net.NetworkInterface
+import java.util.*
 
-class ClientNotify(private val remoteAddress: InetAddress, private val port: Int) {
+
+class ClientNotify(private val port: Int) {
     var socket: DatagramSocket = DatagramSocket()
+    init {
+        socket.broadcast = true
+    }
 
+    /**
+     * send notify about centerNode
+     */
     fun sendNotify() {
         val buf: ByteArray = CodeNotification.Start.code.toByteArray()
-        val packet = DatagramPacket(buf, buf.size, remoteAddress, port)
-
         try {
-            socket.send(packet)
+            val broadcast = listAllBroadcastAddresses()
+            for(remoteAddress in  broadcast) {
+                val packet = DatagramPacket(buf, buf.size, remoteAddress, port)
+                socket.send(packet)
+            }
         } catch (e: IOException) {
             e.printStackTrace()
         }
+    }
+
+    /**
+     * Collect all addresses of networkInterfaces with gateway
+     * send broadcast packet
+     */
+    fun listAllBroadcastAddresses(): List<InetAddress> {
+        val broadcastList = ArrayList<InetAddress>()
+        val interfaces = NetworkInterface.getNetworkInterfaces()
+        while (interfaces.hasMoreElements()) {
+            val networkInterface = interfaces.nextElement()
+
+            if (networkInterface.isLoopback || !networkInterface.isUp) {
+                continue
+            }
+
+            networkInterface.interfaceAddresses.stream()
+                    .map { a -> a.broadcast }
+                    .filter { Objects.nonNull(it) }
+                    .forEach { broadcastList.add(it) }
+        }
+        return broadcastList
     }
 }
--- Notification/src/main/kotlin/EntryPoint.kt
+++ Notification/src/main/kotlin/EntryPoint.kt
@@ -1,8 +1,7 @@
-import java.net.InetAddress
 import io.netty.channel.socket.DatagramPacket as DatagramPacketNetty
 
 fun main(args: Array<String>) {
-    val clientNotify = ClientNotify(InetAddress.getByName("192.168.43.170"), 10001)
+    val clientNotify = ClientNotify( 10001)
     val th = Thread(ServerNotify(10001)).start()
     clientNotify.sendNotify()
     Thread.currentThread().join()
