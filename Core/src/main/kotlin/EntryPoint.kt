a4a67493ae512a164df60eb1ed89d0f833a28a29
konstantin.volivach <konstantin.volivach@eltex.loc>
2019-01-26 15:51:21 +0700

    change structure, add submodules

--- Core/src/main/kotlin/EntryPoint.kt
+++ Core/src/main/kotlin/EntryPoint.kt
@@ -1,8 +1,8 @@
 import io.netty.channel.socket.DatagramPacket as DatagramPacketNetty
 
 fun main(args: Array<String>) {
-    val clientNotify = ClientNotify( 10001)
-    val th = Thread(ServerNotify(10001)).start()
+//    val clientNotify = ClientNotify( 10001)
+//    val th = Thread(ServerNotify(10001)).start()
 //    clientNotify.sendNotify()
-    Thread.currentThread().join()
+//    Thread.currentThread().join()
 }
