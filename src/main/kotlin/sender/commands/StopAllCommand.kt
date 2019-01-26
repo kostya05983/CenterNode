0c013f961fc80d1497b89f6ee5e3649324f02a17
konstantin.volivach <konstantin.volivach@eltex.loc>
2019-01-26 15:10:57 +0700

    change project structure

--- src/main/kotlin/sender/commands/StopAllCommand.kt
+++ src/main/kotlin/sender/commands/StopAllCommand.kt
@@ -5,6 +5,7 @@ import java.net.DatagramSocket
 class StopAllCommand: SendCommand {
     override lateinit var socket: DatagramSocket
     override var port: Int = 1000
+
     override fun execute() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
